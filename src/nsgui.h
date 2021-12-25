e48929b71a0ad90cfd9eafc9450e68bc487a86ed
Joan Karadimov <joan.karadimov@gmail.com>
2021-12-25 12:06:41 +0200

    Extract common gui code to a new header

--- src/nsgui.h
+++ src/nsgui.h
@@ -104,40 +104,6 @@ typedef struct _NSRect  { NSPoint origin; NSSize size; } NSRect;
    (nr).size.height = (pheight))
 
 
-
-
-/* This stuff needed by frame.c.  */
-#define ForgetGravity		0
-#define NorthWestGravity	1
-#define NorthGravity		2
-#define NorthEastGravity	3
-#define WestGravity		4
-#define CenterGravity		5
-#define EastGravity		6
-#define SouthWestGravity	7
-#define SouthGravity		8
-#define SouthEastGravity	9
-#define StaticGravity		10
-
-#define NoValue		0x0000
-#define XValue  	0x0001
-#define YValue		0x0002
-#define WidthValue  	0x0004
-#define HeightValue  	0x0008
-#define AllValues 	0x000F
-#define XNegative 	0x0010
-#define YNegative 	0x0020
-
-#define USPosition	(1L << 0) /* user specified x, y */
-#define USSize		(1L << 1) /* user specified width, height */
-
-#define PPosition	(1L << 2) /* program specified position */
-#define PSize		(1L << 3) /* program specified size */
-#define PMinSize	(1L << 4) /* program specified minimum size */
-#define PMaxSize	(1L << 5) /* program specified maximum size */
-#define PResizeInc	(1L << 6) /* program specified resize increments */
-#define PAspect		(1L << 7) /* program specified min, max aspect ratios */
-#define PBaseSize	(1L << 8) /* program specified base for incrementing */
-#define PWinGravity	(1L << 9) /* program specified window gravity */
+#include "xutilcompat.h"
 
 #endif  /* __NSGUI_H__ */
